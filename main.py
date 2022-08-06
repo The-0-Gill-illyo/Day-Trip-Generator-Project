92d120acd2a2667d17b7fe44f6cb8d3a2b981877
Howard Gill <theogill1369@gmail.com>
2022-08-06 10:42:52 -0400

    Made list in Main.py File

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello world!")
+destinations = ["Disney Land", "Hawaii", "New Zeland", "Hershey Park"]
+restraunts = ["Docking Bay 7 Food and Cargo","Itchy Butt(Chicken Joy)", "The Shed te Motu", "The Chocolate Avenue Grill"]
+trasnsportations = ["Bus", "Cruise", "Plane", "RV"]
+entertainments = ["See Walt Disneys' apartment", "Visit Hawaii Volcano National Park", "Hobbit movie set tour", "Hershey Park Kissing Tower"]
