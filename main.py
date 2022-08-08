ec573efbb2cbd2d64f98f34b98929baa0d3a1f25
Howard Gill <theogill1369@gmail.com>
2022-08-08 18:04:47 -0400

    Modified code, attempted to make code and function working.

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
 import random
 
+print("Hello, allow me to generate a random Day Trip for you")
+
 destinations = ["Disney Land", "Hawaii", "New Zeland", "Hershey Park"]
 restraunts = ["Docking Bay 7 Food and Cargo","Itchy Butt(Chicken Joy)", "The Shed te Motu", "The Chocolate Avenue Grill"]
 trasnsportations = ["Bus", "Cruise", "Plane", "RV"]
 entertainments = ["See Walt Disneys' apartment", "Visit Hawaii Volcano National Park", "Hobbit movie set tour", "Hershey Park Kissing Tower"]
 
-fav_trip = destinations
-fine_dinning = restraunts
-no_fighting = trasnsportations
-that_was_fun = entertainments
+favorite_spot = random.choice(restraunts)
+fine_dinning = random.choice(restraunts)
+no_fighting = random.choice(trasnsportations)
+that_was_fun = random.choice(entertainments)
 
-print(random.choice(fav_trip))
-print(random.choice(fine_dinning))
-print(random.choice(no_fighting))
-print(random.choice(that_was_fun))
+print(random.choice(destinations))
+print(random.choice(restraunts))
+print(random.choice(trasnsportations))
+print(random.choice(entertainments))
+
+input("Would you want to confirm or change these choices? ")
+user_input = sounds_great = "Confirm"
+print(sounds_great)
+user_input = change_something = "Change something"
+print(random.choice(destinations))
+print(random.choice(restraunts))
+print(random.choice(trasnsportations))
+print(random.choice(entertainments))
+
+def make_a_decision(favorite_trip_selection, pick_something_different):
+    return user_input
+print(change_something)
+
+user_input()
+
+# change_something == True
+# print(destinations = random.choice(destinations))
+# print(restraunts = random.choice(restraunts))
+# print(trasnsportations = random.choice(trasnsportations))
+# print(entertainments = random.choice(entertainments))
+
+
+# def favorite_trip_selection(souds_great, change_something):
+#     if souds_great == True:
+#         print("Great, have a wonderful trip!")
+#     elif change_something == False:
+#         print(destinations = random.choice(destinations))
+#         print(restraunts = random.choice(restraunts))
+#         print(trasnsportations = random.choice(trasnsportations))
+#         print(entertainments = random.choice(entertainments))
+#         return favorite_trip_selection
+#         print(favorite_trip_selection)
+
+# favorite_trip_selection()
+
+
+# fav_trip = False
+# trip_choice = False
+
+# display trip to user
+# prompt user t confirm or change trip?
+# 
+
+
+
+
+
+# def user_choice():
+
+#     fav_trip == user_choice
+#     input("Do you like these options?")
+#     while fav_trip in trip_choice == True:
+#         if trip_choice == fav_trip:
+#             print("Sounds great, have a blast!")
+#         elif trip_choice != fav_trip:
+#             print("which item would you like to change?")
+#             new_choice = fav_trip(random.choice)
+#             return new_choice
+#             print(user_choice)
+
+#         user_choice()
