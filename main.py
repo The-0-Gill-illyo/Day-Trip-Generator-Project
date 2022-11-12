98ce1d8081d8aefd85048cb5a3f694697db15d12
The-0-Gill-illyo <theogill1369@gmail.com>
2022-11-12 17:16:15 -0500

    Finished Day trip Project

--- main.py
+++ main.py
@@ -1,104 +1,52 @@
 import random
 
 
-destinations = ["Disney Land", "Hawaii", "New Zeland", "Hershey Park"]
-restraunts = ["Docking Bay 7 Food and Cargo","Itchy Butt(Chicken Joy)", "The Shed te Motu", "The Chocolate Avenue Grill"]
-transportations = ["Bus", "Cruise", "Plane", "RV"]
-entertainments = ["See Walt Disneys' apartment", "Visit Hawaii Volcano National Park", "Hobbit movie set tour", "Hershey Park Kissing Tower"]
-
-print("Hello, allow me to generate a random Day Trip for you") 
-
-def make_a_decision(destinations, restraunts, transportations, entertainments):
-    day_trip = destinations + restraunts + transportations + entertainments
-    user_yes = True
-    no_way = False
-    print(random.choice(destinations))
-
-    print(random.choice(restraunts))
-    print(random.choice(transportations))
-    print(random.choice(entertainments))
-    input("Do you like this trip, or would you like to change something? Yes or No? ")
-    # while user_yes in make_a_decision == True:
-    if(no_way) == day_trip:
-            print("Great, you'll enjoy what's planned!")
-    elif user_yes == day_trip:
-            print("What would you like to change?")
-
-            return day_trip
-    
-    result_from_user = make_a_decision(destinations, restraunts, transportations, entertainments)
-    
-    
-    
-    
-    
-    #     return make_a_decision()
-    # day_trip()
-            # fine_dinning = random.choice(restraunts)
-    # no_fighting = random.choice(trasnsportations)
-    # that_was_fun = random.choice(entertainments)
-    # return result_journey
-    
-
-
-# input("Would you want to confirm or change these choices? ")
-# user_input = sounds_great = "Confirm"
-# print(sounds_great)
-# user_input = change_something = "Change something"
-# print(random.choice(destinations))
-# print(random.choice(restraunts))
-# print(random.choice(trasnsportations))
-# print(random.choice(entertainments))
-
-# def make_a_decision(favorite_spot, fine_dinning, no_fighting, that_was_fun):
-#     result_journey = favorite_spot + fine_dinning + no_fighting + that_was_fun
-#     return result_journey
+destinations = ["Disney Land Cruise!", "Hawaii!", "New Zeland!", "Hershey Park!"]
+restraunts = ["Docking Bay 7 Food and Cargo, at Disney Land!","Itchy Butt(Chicken Joy), in Honolulu!", "The Shed te Motu, over looking the water in New Zeland!", "The Chocolate Avenue Grill, in Hersey Pennsylvania!"]
+transportations = ["Celebry Bus Tour!", "10 Day/9 Night Cruise!", "First-Class Plane!", "RV Cross-County Trip!"]
+entertainments = ["See Walt Disneys' apartment!", "Visit Hawaii Volcano National Park!", "Hobbit movie set tour!", "Hershey Park Kissing Tower!"]
 
 
 
-# change_something == True
-# print(destinations = random.choice(destinations))
-# print(restraunts = random.choice(restraunts))
-# print(trasnsportations = random.choice(trasnsportations))
-# print(entertainments = random.choice(entertainments))
+user_choosen_destinatation = random.choice(destinations)
+user_choosen_restraunt = random.choice(restraunts)
+user_choosen_transportation = random.choice(transportations)
+user_choosen_entertainment = random.choice(entertainments)
 
 
-# def favorite_trip_selection(souds_great, change_something):
-#     if souds_great == True:
-#         print("Great, have a wonderful trip!")
-#     elif change_something == False:
-#         print(destinations = random.choice(destinations))
-#         print(restraunts = random.choice(restraunts))
-#         print(trasnsportations = random.choice(trasnsportations))
-#         print(entertainments = random.choice(entertainments))
-#         return favorite_trip_selection
-#         print(favorite_trip_selection)
+def display_travel_choices():
+        print("Vacation Destination: " + user_choosen_destinatation)
+        print("Dining Experience: " + user_choosen_restraunt)
+        print("Travel Type: " + user_choosen_transportation)
+        print("Tours and Vacation Sites: " + user_choosen_entertainment)
 
-# favorite_trip_selection()
+print("Hello, allow me to generate a random Day Trip for you")
 
+confimed_trip = False
 
-# fav_trip = False
-# trip_choice = False
+while not confimed_trip:
+        display_travel_choices()
 
-# display trip to user
-# prompt user t confirm or change trip?
-# 
+        respose = input("Would you like to go on the journey? yes or no   ")
+        print(respose)
 
+        if respose == "yes":
+                print(f'Fantastic, Enjoy your trip at {user_choosen_destinatation}! The attractions at {user_choosen_entertainment}, accompanied with an excellent dining experience at {user_choosen_restraunt}. Please remember to travel safely and follow all third-party companies while traveling by {user_choosen_transportation}! Have a great trip!')
+                confimed_trip = True
+        elif respose == "no":
+                print("Destination")
+                print("Restraunt")
+                print("Transportation")
+                print("Entertainment")
+                respose = input("Which would you like to change? ")
 
-
-
-
-# def user_choice():
-
-#     fav_trip == user_choice
-#     input("Do you like these options?")
-#     while fav_trip in trip_choice == True:
-#         if trip_choice == fav_trip:
-#             print("Sounds great, have a blast!")
-#         elif trip_choice != fav_trip:
-#             print("which item would you like to change?")
-#             new_choice = fav_trip(random.choice)
-#             return new_choice
-#             print(user_choice)
-
-#         user_choice()
+                if respose == "Destination":
+                        user_choosen_destinatation = random.choice(destinations)
+                elif respose == "Restraunt":
+                        user_choosen_restraunt = random.choice(restraunts)
+                elif respose == "Transportation":
+                        user_choosen_transportation = random.choice(transportations)
+                elif respose == "Entertainment":
+                        user_choosen_entertainment = random.choice(entertainments)
+        else:
+                print("Invalid please try again")
